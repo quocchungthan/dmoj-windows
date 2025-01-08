5808f866ef51b89730ee9ffdfd01db07564f3818
chung <quocchung.than@gmail.com>
2025-01-08 14:22:58 +0700

    should work fine on windows local

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,7 @@
 docker-compose up --build -d web
+docker container restart dmoj_app
+REM remove the echo line if your machien does not have python
+echo from time import sleep; sleep(40) | python
 docker exec -it dmoj_app /app/load_data.sh
 docker-compose up --build -d
 docker exec -it dmoj_site /app/load_assets.sh
