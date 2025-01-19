ccd65f99a885a8fb991e8b4dab06603e35be2e70
chung <quocchung.than@gmail.com>
2025-01-19 10:06:38 +0700

    allow localhost

--- idzhost.vn-haitin.edu.vn/local_settings.py
+++ idzhost.vn-haitin.edu.vn/local_settings.py
@@ -17,7 +17,7 @@ DEBUG = True  # Change to False once you are done with runserver testing.
 
 # Uncomment and set to the domain names this site is intended to serve.
 # You must do this once you set DEBUG to False.
-ALLOWED_HOSTS = ['<DEPLOYMENT-DOMAIN>', '<SERVER-ADDRESS>']
+ALLOWED_HOSTS = ['<DEPLOYMENT-DOMAIN>', '<SERVER-ADDRESS>', '127.0.0.1']
 
 # Optional apps that DMOJ can make use of.
 INSTALLED_APPS += (
